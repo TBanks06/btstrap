656ce4639ae4705831a83dce91079501d09bac63
Kazeem Bankole <tunjibankole@gmail.com>
2023-03-30 10:31:57 +0100

    updates

--- src/NewG.jsx
+++ src/NewG.jsx
@@ -6,8 +6,8 @@ function NewG() {
     <Card className='border-0' style={{ width: '24rem' } }>
       <Card.Body className='bg-info rounded-4 '>
         
-        <Card.Title className='mb-4 display-5 fw-normal font-monospace'>New goals  require new knowledge</Card.Title>
-            <Card.Text className='mb-5 fs-6 text-sm-start fw-light lh-1' style={{width: '13rem'}} >
+        <Card.Title className='mb-5 display-5 fw-normal font-monospace'>New goals  require new knowledge</Card.Title>
+            <Card.Text className='mb-4 fs-6 text-sm-start fw-light lh-1' style={{width: '13rem'}} >
 
                 We have created an e-learning platform that allows teams to find, share, 
                 and use the knowledge, they need to achieve their goals.
